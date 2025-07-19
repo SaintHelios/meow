c04c9af1b1adc09a9e1eeba7a31700c99a4da06f
SaintHelios <184778372+SaintHelios@users.noreply.github.com>
2025-07-19 13:35:02 +0300

    perhaps mayhaps perchance

--- modules/home-manager/programs/waybar/default.nix
+++ modules/home-manager/programs/waybar/default.nix
@@ -5,32 +5,9 @@ let
 in {
   programs.waybar.enable = true;
 
-  home.file."${waybarDir}/config.jsonc" = {
-    source = ./config.jsonc;
-  };
-
-  home.file."${waybarDir}/style.css" = {
-    source = ./style.css;
-  };
-
-  home.file."${waybarDir}/scripts/app_launcher.sh" = {
-    source = ./scripts/app_launcher.sh;
-  };
-
-  home.file."${waybarDir}/scripts/get_window.sh" = {
-    source = ./scripts/get_window.sh;
-  };
-
-  home.file."${waybarDir}/scripts/launch.sh" = {
-    source = ./scripts/launch.sh;
-  };
-
-  home.file."${waybarDir}/scripts/weather.sh" = {
-    source = ./scripts/weather.sh;
-  };
-
-  home.file."${waybarDir}/scripts/nmtui.sh" = {
-    source = ./scripts/nmtui.sh;
+  home.file."${waybarDir}" = {
+    source = ./.;
+    recursive = true;
   };
 
   home.activation.makeWaybarScriptsExecutable = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
