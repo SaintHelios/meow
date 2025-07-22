f0b4114de353781f8effb3aae5bd16c7368b0b39
SaintHelios <184778372+SaintHelios@users.noreply.github.com>
2025-07-22 23:13:53 +0300

    cleaned up unneccessary modules

--- modules/nixos/desktop/greetd/default.nix
+++ modules/nixos/desktop/greetd/default.nix
@@ -6,25 +6,6 @@
     hyprland
   ];
 
-  # Deploy the tuigreet theme config TOML file system-wide
-  environment.etc."tuigreet/config.toml".text = ''
-    [style]
-    background_color = "black"
-    foreground_color = "white"
-    highlight_color = "cyan"
-    error_color = "red"
-
-    [font]
-    family = "monospace"
-    size = 12
-
-    [layout]
-    welcome_text = "Welcome to Hyprland!"
-    show_asterisks = true
-    show_user_menu = true
-  '';
-
-  # Enable greetd and configure it to use tuigreet as the greeter
   services.greetd = {
     enable = true;
     settings = {
@@ -35,7 +16,6 @@
     };
   };
 
-  # Configure the greetd systemd service (optional but recommended)
   systemd.services.greetd.serviceConfig = {
     Type = "idle";
     StandardInput = "tty";
@@ -46,7 +26,6 @@
     TTYVTDisallocate = true;
   };
 
-  # Define the greeter system user with home directory and bash shell
   users.users.greeter = {
     isSystemUser = true;
     createHome = true;
